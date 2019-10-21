ee95a02d284379420667f8af54a09995e389d4f0
waterylime46@aol.com <waterylime46@aol.com>
2019-10-21 10:42:45 -0600

    code complete not working

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
- // This is our API key
- var APIKey = "500d26746e8c4e16d7bb84617a694bdf";
+//  // This is our API key
+//  var APIKey = "500d26746e8c4e16d7bb84617a694bdf";
 
- // Here we are building the URL we need to query the database
- var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name},{country code}" + APIKey;
+//  // Here we are building the URL we need to query the database
+//  var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name},{country code}" + APIKey;
   
- // Here we run our AJAX call to the OpenWeatherMap API
- $.ajax({
-   url: queryURL,
-   method: "GET"
- })
-   // We store all of the retrieved data inside of an object called "response"
-   .then(function(response) {
+//  // Here we run our AJAX call to the OpenWeatherMap API
+//  $.ajax({
+//    url: queryURL,
+//    method: "GET"
+//  })
+//    // We store all of the retrieved data inside of an object called "response"
+//    .then(function(response) {
 
-     // Log the queryURL
-     console.log(queryURL);
+//      // Log the queryURL
+//      console.log(queryURL);
 
-     // Log the resulting object
-     console.log(response);
+//      // Log the resulting object
+//      console.log(response);
 
-     // Transfer content to HTML
-     $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-     $(".wind").text("Wind Speed: " + response.wind.speed);
-     $(".humidity").text("Humidity: " + response.main.humidity);
-     $(".temp").text("Temperature (F) " + response.main.temp);
+//      // Transfer content to HTML
+//      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+//      $(".wind").text("Wind Speed: " + response.wind.speed);
+//      $(".humidity").text("Humidity: " + response.main.humidity);
+//      $(".temp").text("Temperature (F) " + response.main.temp);
 
-     // Log the data in the console as well
-     console.log("Wind Speed: " + response.wind.speed);
-     console.log("Humidity: " + response.main.humidity);
-     console.log("Temperature (F): " + response.main.temp);
-   });
+//      // Log the data in the console as well
+//      console.log("Wind Speed: " + response.wind.speed);
+//      console.log("Humidity: " + response.main.humidity);
+//      console.log("Temperature (F): " + response.main.temp);
+//    });
--- weather.js
+++ weather.js
@@ -1,29 +1,75 @@
-window.addEventListener('load', ()=>{
+// $(document).ready(() => {
 
 
-    let long;
-    let lat;
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-long = position.coords.longitude;
-lat = position.coords.latitude;
 
-var APIKey = "500d26746e8c4e16d7bb84617a694bdf";
-const api = `http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139` + APIKey;
 
-fetch(api)
-        .then(response =>{
-            return response.json();
-        })
+$("#searchCity").on("click", (event) => {
+    event.preventDefault();
+    $("#cityView").empty();
+    var city = $("#cityInput").val().trim();
+    localStorage.setItem("city", city);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b72b06e94024103dcc49ba18cc9af972";
+    
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then((response) =>{
+        console.log(response)
 
-        .then(data =>{
-console.log(data)
-        })
-        });
+    var cityDiv =$("<div class='city'>");
+    var cityName = response.name;
+    var h1 = $("<h1>").html(cityName + " (" + moment().format("L") + ")" + "<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+    cityDiv.append(h1);
+    var temperature = response.main.temp;
+    var pTwo = $("<p>").html("Temperature: " + Math.round((temperature - 273.15) * 1.80 + 32) + " ℉");
+    cityDiv.append(pTwo);
+    var humidity = response.main.humidity;
+    var pThree = $("<p>").html("Humidity: " + humidity + " %");
+    cityDiv.append(pThree);
+    var windSpeed = response.wind.speed;
+    var pFour = $("<p>").html("Wind speed: " + windSpeed + " MPH");
+    cityDiv.append(pFour);
+  
+    $("#cityView").prepend(cityDiv);
+    
+    })
+    
+    fiveDayForecast(city);
+
+    var button = $("<button>").addClass("buttons");
+    button.prepend(localStorage.getItem("city"))
+    $("#searchHistory").append(button);
+    
+})
+
+function fiveDayForecast (city) {
+    var queryURL5 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=b72b06e94024103dcc49ba18cc9af972";
+    $.ajax({
+        url: queryURL5,
+        method: "GET"
+    }).then((forecast) => {
+        console.log(forecast);
+        $(".forecastView").empty();
         
-        
-    }
-
-
-});
+        var time = "12:00:00";
+        for (var i = 0; i < forecast.list.length; i ++) {
+            var forecastTime = forecast.list[i].dt_txt;
+            console.log(forecastTime)
+            if (forecastTime.indexOf(time) !== -1) {
+                
+                var splitDate = forecast.list[i].dt_txt.split(" ");
+                var newDate = splitDate[0].slice(0, 10).split("-");
+                cardDiv = $("<div>").addClass("card");
+                pOne = $("<p>").html(newDate[1] + "/" + newDate[2] + "/" + newDate[0]);
+                pTwo = $("<p>").html("<img src='http://openweathermap.org/img/w/" + forecast.list[i].weather[0].icon + ".png' alt='Icon depicting current weather'>") 
+                pThree = $("<p>").html("Temp: " + Math.round((forecast.list[i].main.temp - 273.15) * 1.80 + 32) + " ℉");
+                pFour = $("<p>").html("Humidity: " + forecast.list[i].main.humidity + " %");
+                cardDiv.append(pOne, pTwo, pThree, pFour);
+                $(".forecastView").append(cardDiv);
+                
+            }
+        }
+    })
+}
+// });
